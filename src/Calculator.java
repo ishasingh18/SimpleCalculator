442d903002ae4bac78791a736cae82c43920f636
ishasingh18 <singhisha1804@gmail.com>
2024-12-25 23:31:39 +0000

    created menu-driven calculator

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public int add(int a, int b) {
+        int ans = a + b;
+        return ans;
+    }
+    public int subtract(int a,int b){
+        int ans = a - b;
+        return ans;
+    }
+    public int multiply(int a, int b){
+        int ans = a * b;
+        return ans;
+    }
+    public int divide(int a, int b){
+        int ans = a / b;
+        return ans;
+    }
+}
