8d2c9969d8005c6389c245a73eb0bfda77efe28c
ishasingh18 <singhisha1804@gmail.com>
2024-12-25 23:29:40 +0000

    2

--- src/Calculator.java
+++
@@ -1,7 +0,0 @@
-public class Calculator {
-    public void add(int a,int b){
-        int sum=0;
-        sum= a+b;
-        System.out.println("Sum = "+sum);
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        SimpleCalc obj = new SimpleCalc();//Creating object for class
-        //Calling all methods
-       int n = obj.getOperands();
-       double numbers[] = obj.getNumbers(n);
-       double answer = obj.performOperations(n,numbers);
-       obj.printOutput(answer);
-
-    }
+        Scanner sc=new Scanner(System.in);
+        Calculator obj = new Calculator();
+        System.out.println("Choose an operation");
+        System.out.println("1.Add");
+        System.out.println("2.Subtract");
+        System.out.println("3.Multiply");
+        System.out.println("4.Divide");
+        int n=sc.nextInt();
+        System.out.println("Enter 2 numbers");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        switch(n){
+            case 1:{
+                System.out.println("Answer = "+ obj.add(x,y));
+                break;
+            }
+            case 2:{
+                System.out.println("Answer = "+ obj.subtract(x,y));
+                break;
+            }
+            case 3:{
+                System.out.println("Answer = "+ obj.multiply(x,y));
+                break;
+            }
+            case 4:{
+                System.out.println("Answer = "+ obj.divide(x,y));
+                break;
+            }
+        }
+   }
 }
