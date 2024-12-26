1d681e56d12076e181c806213511138fe7a6587f
ishasingh18 <singhisha1804@gmail.com>
2024-12-26 00:16:41 +0000

    created menu-driven calculator added comments

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Calculator obj = new Calculator();
-        System.out.println("Choose an operation");
+        int x=0;
+        int y=0;
+        Calculator obj = new Calculator(); //creating object of class
+        System.out.println("Choose an operation"); //creating menu
         System.out.println("1.Add");
         System.out.println("2.Subtract");
         System.out.println("3.Multiply");
         System.out.println("4.Divide");
-        int n=sc.nextInt();
-        System.out.println("Enter 2 numbers");
-        int x=sc.nextInt();
-        int y=sc.nextInt();
+        int n=sc.nextInt(); //taking option from user
+        if(n<1 || n>4){
+            System.out.println("Invalid input!! Please try again");
+            System.exit(0);
+        }
+        else{
+            System.out.println("Enter 2 numbers");
+             x=sc.nextInt();
+             y=sc.nextInt();
+        }
+
         switch(n){
             case 1:{
-                System.out.println("Answer = "+ obj.add(x,y));
+                System.out.println("Answer = "+ obj.add(x,y)); //method call
                 break;
             }
             case 2:{
@@ -26,7 +35,13 @@ public class Main {
                 break;
             }
             case 4:{
-                System.out.println("Answer = "+ obj.divide(x,y));
+                if(y==0){
+                    System.out.println("Can't divide by 0!");
+                    System.exit(0);
+                }
+                else{
+                    System.out.println("Answer = "+ obj.divide(x,y));
+                }
                 break;
             }
         }
