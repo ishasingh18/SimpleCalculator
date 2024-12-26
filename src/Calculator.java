93d2446c336bfb859e243b7c537432eeeb16f6db
ishasingh18 <singhisha1804@gmail.com>
2024-12-26 00:17:45 +0000

    created menu-driven calculator added comments handled division by 0

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
-    public int add(int a, int b) {
+    public int add(int a, int b) { //method to add numbers
         int ans = a + b;
-        return ans;
+        return ans;//returning output
     }
-    public int subtract(int a,int b){
+    public int subtract(int a,int b){ //method to subtract numbers
         int ans = a - b;
         return ans;
     }
-    public int multiply(int a, int b){
+    public int multiply(int a, int b){ //method to multiply numbers
         int ans = a * b;
         return ans;
     }
-    public int divide(int a, int b){
+    public int divide(int a, int b){ //method to divide numbers
         int ans = a / b;
         return ans;
     }
